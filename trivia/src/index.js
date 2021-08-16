d3835c997ad7dba7c2d730c82370bd476f8f64f8
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-08-16 18:05:34 -0300

    Modificando Router

--- trivia/src/index.js
+++ trivia/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <HashRouter>
+    <BrowserRouter basename="/trivia">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
