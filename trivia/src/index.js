df96983b88188fb0198d5af11ff9c27f75bbd039
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-08-17 19:46:17 -0300

    VOltando para hash

--- trivia/src/index.js
+++ trivia/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './store';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter basename="/trivia">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
